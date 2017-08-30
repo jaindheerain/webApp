d546aef6165a15c9753b6d72e4380cc9ff574a12
dheerain-jain <jain.dheerain@gmail.com>
2017-08-30 17:47:30 +0530

    ArgvSetup;ErrorHandled;Jsonreceived

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 var request=require('request');
+var yargs=require('yargs');
+
+var argv=yargs.
+        options({
+        a:{
+         demand:true,
+            string:true,
+            alias:'address',
+            describe:'Th address of the location you want to get the weather information about'
+        }
+        })
+        .help()
+        .argv;
+
+
+const url=encodeURIComponent(argv.a);
+
+console.log(url);
 
 request({
-    url:"http://maps.googleapis.com/maps/api/geocode/json?address=Jaypee%20insitute%20of%20information%20technology",
+    url:`http://maps.googleapis.com/maps/api/geocode/json?address=${url}`,
     json:true
-},(err,request,body)=>{
-    var address=body.results[0].formatted_address;
-    var lat=body.results[0].geometry.location.lat;
-    var lan=body.results[0].geometry.location.lng;
-    console.log(address);
-    console.log(`Latitiue : ${lat}`);
-    console.log(`Latitiue : ${lan}`);
+    },(err,request,body)=>{
+    if(err){
+        console.log("Unable to connecto the Google Servers.")
+    }else if(body.status==='ZERO_RESULTS'){
+        console.log("Unable to get the Adress asked for. Address Doenat Exists ")
+    }else if(body.status==='OK'){
 
-})
+        var address=body.results[0].formatted_address;
+        var lat=body.results[0].geometry.location.lat;
+        var lan=body.results[0].geometry.location.lng;
+        console.log(address);
+        console.log(`Latitiue : ${lat}`);
+        console.log(`Latitiue : ${lan}`);
+    }
+
+
+});
 
